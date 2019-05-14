0b7b2eb32f5eb7feba6d62f9465915c7692f7a77
Bruce Hamilton <bruce.hamilton@fairfaxmedia.co.nz>
2019-05-15 10:31:36 +1200

    Removed howler.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {Howl} from 'howler';
 
 @Component({
   selector: 'app-root',
@@ -8,31 +7,26 @@ import {Howl} from 'howler';
 })
 export class AppComponent {
 
-  howl: Howl;
+  audio: HTMLAudioElement;
 
   shortLoop(): void {
-    if (this.howl) {
-      this.howl.unload();
+    if (this.audio) {
+      this.audio.pause();
     }
-    this.howl = new Howl({
-      src: 'assets/short-loop.mp3',
-      loop: true,
-      autoplay: true,
-      html5: true,
-    });
+    this.audio = new Audio();
+    this.audio.src = 'assets/short-loop.mp3';
+    this.audio.loop = true;
+    this.audio.play();
   }
 
   longLoop(): void {
-    if (this.howl) {
-      this.howl.unload();
+    if (this.audio) {
+      this.audio.pause();
     }
-    this.howl = new Howl({
-      src: 'assets/long-loop.mp3',
-      loop: true,
-      autoplay: true,
-      html5: true,
-    });
-    this.howl.fade(0, 1, 3000);
+    this.audio = new Audio();
+    this.audio.src = 'assets/long-loop.mp3';
+    this.audio.loop = true;
+    this.audio.play();
   }
 
 }
